2a18d671608d5ca7c478359fa27b21c78fed495f
Hien Nguyen <ntnhien13@gmail.com>
2024-05-15 14:31:43 +0700

    Hero styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,15 +5,18 @@ import pfp from "../public/avar.jpg"
 
 export default function Hero() {
     return(
-        <section id="hero-container">
+        <section className="h-svh flex justify-center items-center flex-col py-8 px-0" id="hero-container">
             <Image
                 src={pfp}
                 alt="Profile picture"
+                className="w-200px h-200px rounded-full my-o mx-auto object-cover"
             />
-            <div className="hero-text">
-                <h1>Hi, I'm Hien Nguyen</h1>
-                <p>
-                I'm a full-stack developer in Ho Chi Minh City, Vietnam.
+            <div className="text-center my-4 mx-0 w-4/5" id="hero-text">
+                <h1 className="text-4xl font-semibold mb-4">
+                    Hi, I&#39;m Hien Nguyen
+                </h1>
+                <p className="text-lg font-normal mb-4">
+                    I'm a full-stack developer in Ho Chi Minh City, Vietnam.
                 </p>
                 <div className="social-icons">
                     Socials
