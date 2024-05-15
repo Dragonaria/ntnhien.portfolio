be577d0ba446267d18a334907b9ce73c73305cdb
Hien Nguyen <ntnhien13@gmail.com>
2024-05-15 17:22:17 +0700

    Add socials to Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,8 +8,25 @@ export default function Footer() {
                 <p className="text-slate-400">
                     {new Date().getFullYear()} Hien Nguyen&#39;s Portfolio
                 </p>
-                <div className="social_icons">
-                    Socials
+                <div className="flex justify-between items-center gap-y-0 gap-x-4" id="footer-socials">
+                    <a
+                        href="https://www.linkedin.com/in/ntnhien"
+                        aria-label="LinkedIn"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-slate-400 transition-all duration-300 ease-in-out hover:text-slate-100"
+                    >
+                        <i className='bx bxl-linkedin-square bx-md' ></i>
+                    </a>
+                    <a
+                        href="https://github.com/Dragonaria"
+                        aria-label="GitHub"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-slate-400 transition-all duration-300 ease-in-out hover:text-slate-100"
+                    >
+                        <i className='bx bxl-github bx-md' ></i>
+                    </a>
                 </div>
             </div>
         </footer>
